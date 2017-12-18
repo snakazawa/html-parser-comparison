21c2f0d47dda4df2c819a5d46ccdea9bb3d0ee8d
snakazawa <nakazawa.tms0314@gmail.com>
2017-12-18 23:26:20 +0900

    :new: add cpp gumbo-query

--- README.md
+++ README.md
@@ -41,4 +41,30 @@ python beautifulsoup4_html5lib.py
 
 ## Parse by C++
 
-**TODO**
+### gumbo-query
+
+```
+# install gumbo-parser
+cd ~
+git@github.com:google/gumbo-parser.git
+cd gumbo-parser
+./autogen.sh
+./configure
+make
+sudo make install
+sudo /sbin/ldconfig
+
+# install gumbo-query
+cd ~
+git clone git@github.com:lazytiger/gumbo-query.git
+cd gumbo-query/build
+cmake ..
+make
+sudo make install
+sudo /sbin/ldconfig
+
+# test
+cd compare_html_parser/cxx
+g++ -std=c++1y -O2 gumbo_test.cpp -o gumbo_test.out -I/usr/local/include -L/usr/local/lib -lgumbo -lgq
+./gumbo_test
+```
---
+++ cxx/gumbo_test.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iterator>
+#include <vector>
+#include <sys/types.h>
+#include <dirent.h>
+#include <gq/Document.h>
+#include <gq/Node.h>
+#include "ProgressBar.hpp"
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    string dir_path("../data/pages/");
+    DIR* dp = opendir(dir_path.c_str());
+
+    if (dp == NULL) {
+        cerr << "cannot open dir: " << dir_path << endl;
+        exit(1);
+    }
+
+    vector<string> filenames;
+    struct dirent* dent;
+    while (true) {
+        dent = readdir(dp);
+        if (dent == NULL) break;
+
+        string filename(dent->d_name);
+        if (filename.find(".") == 0) continue;
+
+        filenames.push_back(filename);
+    }
+
+    closedir(dp);
+
+    constexpr int PROGRESSBAR_WIDTH = 70;
+    ProgressBar progress_bar(filenames.size(), PROGRESSBAR_WIDTH);
+
+    for (auto filename : filenames) {
+        string path = dir_path + filename;
+        ifstream ifs(path);
+        if (ifs.fail()) {
+            cerr << "cannot open file: " << path << endl;
+            exit(1);
+        }
+
+        istreambuf_iterator<char> it(ifs);
+        istreambuf_iterator<char> last;
+        string text(it, last);
+
+        CDocument doc;
+        doc.parse(text.c_str());
+
+        //CSelection c = doc.find("h1");
+        //if (c.nodeNum() == 0) continue;
+        //cout << c.nodeAt(0).text() << std::endl; // some link
+
+        ++progress_bar;
+        progress_bar.display();
+    }
+
+    progress_bar.done();
+
+    return 0;
+}
